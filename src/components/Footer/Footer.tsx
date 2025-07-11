e57071475b7530f2ae594976e06937ddff7373c1
Praanjal Joshi <pranjalj967@gmail.com>
2025-07-11 16:02:17 +0530

    Full and Final

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,30 +4,30 @@ const Footer = () => {
   return (
     <footer className="bg-[#0F172A] text-white py-5 px-1 border-t  border-white relative">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center gap-6">
-
         {/* Contact Info */}
         <div className="text-center md:text-left">
-          <h3 className="text-xl font-semibold mb-2 select-none">Get in Touch</h3>
+          <h3 className="text-xl font-semibold mb-2 select-none">
+            Get in Touch
+          </h3>
           <p className="flex gap-2 items-center select-none">
             Email:{" "}
             <a
-              href="mailto:your.email@example.com"
+              href="mailto:pranjalj967@gmail.com"
               className="underline hover:text-purple-300 transition select-none"
             >
-              <FaEnvelope/>
+              <FaEnvelope />
             </a>
           </p>
           <p className="select-none">Location: India</p>
         </div>
 
         <div className="text-center md:text-left">
-            <p className="text-sm opacity-30 select-none">©2025 Praanjal Joshi</p>
+          <p className="text-sm opacity-30 select-none">©2025 Praanjal Joshi</p>
         </div>
-
 
         <div className="flex gap-6 text-2xl">
           <a
-            href="https://github.com/yourusername"
+            href="https://github.com/pjcodes007"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-purple-400 transition"
@@ -36,7 +36,7 @@ const Footer = () => {
           </a>
 
           <a
-            href="https://linkedin.com/in/yourprofile"
+            href="www.linkedin.com/in/praanjal-joshi-84406629b"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-purple-400 transition"
@@ -45,7 +45,7 @@ const Footer = () => {
           </a>
 
           <a
-            href="https://instagram.com/yourhandle"
+            href="https://instagram.com/Praanjal_Joshi"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-pink-400 transition"
@@ -54,7 +54,9 @@ const Footer = () => {
           </a>
         </div>
       </div>
-         <div className="text-white text-5xl absolute bottom-1 left-0 opacity-7 font-[monospace] font-extrabold select-none ">Footer</div>
+      <div className="text-white text-5xl absolute bottom-1 left-0 opacity-7 font-[monospace] font-extrabold select-none ">
+        Footer
+      </div>
     </footer>
   );
 };
